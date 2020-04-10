3f72fb47500e6365a91f710f149108aaeaabd1d6
Sandman <antonio.lima16@fatec.sp.gov.br>
2020-04-10 12:36:13 -0300

    Upload de imagem funcionando utilizando o BusBoy, autenticação e validação de tipo de arquivo também implementada.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions')
 const app = require('express')()
 const { getAllScreams, postOneScream } = require('./handlers/screams')
-const {signup, login} = require('./handlers/users')
+const {signup, login, uploadImage} = require('./handlers/users')
 const FBAuth = require('./util/fbAuth')
 
 // Scream routes
@@ -12,5 +12,7 @@ app.post('/scream',FBAuth, postOneScream)
 app.post('/signup', signup)
 // login route
 app.post('/login', login)
+// upload Image Route
+app.post('/user/image', FBAuth, uploadImage)
 
 exports.api = functions.region('us-east1').https.onRequest(app)
