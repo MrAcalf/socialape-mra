207ff192cbc6a200c06320d4eee3b283065f2c59
Sandman <antonio.lima16@fatec.sp.gov.br>
2020-05-05 17:17:10 -0300

    Versão final do projeto online

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions')
 const app = require('express')()
-const { db } = require('./util/admin')
+const cors = require('cors')
+app.use(cors())
+const { db, admin } = require('./util/admin')
+
 const { 
     getAllScreams, 
     postOneScream, 
